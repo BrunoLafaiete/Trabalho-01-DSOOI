6210dcb4bdb4c021908357bca41a073e16d76034
JoaoPedro2002 <80429973+JoaoPedro2002@users.noreply.github.com>
2021-03-26 16:58:23 -0300

    Update desenvolvedora.py

--- entidade/desenvolvedora.py
+++ entidade/desenvolvedora.py
@@ -1,5 +1,24 @@
-
+from entidade.jogo import Jogo
 
 
 class Desenvolvedora:
-    pass
+
+    def __init__(self, nome):
+        self.__nome = nome
+        self.__jogos = []
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def jogos(self):
+        return self.__jogos
+
+    def incluir_jogo(self, jogo: Jogo):
+        self.__jogos.append(jogo)
+        jogo.desenvolvedora(self)
