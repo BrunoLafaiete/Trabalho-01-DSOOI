8b4489cd18d9dc559a12740c3c486820d931049b
JoaoPedro2002 <80429973+JoaoPedro2002@users.noreply.github.com>
2021-03-31 19:40:08 -0300

    Abstração e exceções

--- limite/tela_jogo.py
+++ limite/tela_jogo.py
@@ -1,7 +1,8 @@
+from limite.tela_abstrata import TelaAbstrata
+from excecoes.nome_invalido_exception import NomeInvalidoException
 
 
-
-class TelaJogo:
+class TelaJogo(TelaAbstrata):
 
     def tela_opcoes(self):
         print("------JOGO------")
@@ -10,33 +11,92 @@ class TelaJogo:
         print("2 - Alterar dados do jogo")
         print("3 - Pegar dados do jogo")
         print("4 - Listar jogos")
+        print("0 - Voltar")
         print()
-        opcao_escolhida = int(input("Escolha uma opção: "))
+        opcao_escolhida = self.le_num_int("Escolha uma opção: ", [0, 1, 2, 3, 4])
         return opcao_escolhida
 
-    def cadastrar_jogo(self):
+    def cadastrar_jogo(self, jogos):
         print("-----CADASTRAR UM JOGO-----")
-        nome = input("Nome: ")
+        while True:
+            nome = input("Nome: ")
+            try:
+                for jogo in jogos:
+                    if jogo.nome == nome:
+                        raise NomeInvalidoException
+                break
+            except NomeInvalidoException as e:
+                print(e)
+
         desenvolvedora = input("Desenvolvedora: ")
         genero = input("Genero: ")
-        faixa_etaria = input("Faixa etaria: ")
-        preco = input("Preco: ")
+        while True:
+            faixa_etaria = input("Faixa etaria (0 ate 100): ")
+            try:
+                faixa_etaria = int(faixa_etaria)
+                if faixa_etaria > 100 or faixa_etaria < 0:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Selecione um valor inteiro valido")
+
+        while True:
+            preco = input("Preco (0 ate 10000): ")
+            try:
+                preco = float(preco)
+                if preco > 10000 or preco < 0:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Selecione um valor valido")
 
         return {"nome": nome, "desenvolvedora": desenvolvedora, "genero": genero,
-                "faixa etaria": int(faixa_etaria), "preco": float(preco)}
+                "faixa etaria": faixa_etaria, "preco": preco}
 
-    def alterar_jogo(self):
+    def alterar_jogo(self, jogos, nome_antigo):
         print("-----ALTERAR DADOS-----")
-        print("Vamos buscar um jogo para alterar...")
-        nome_antigo = input("Nome do Jogo: ")
-        return nome_antigo
+        while True:
+            nome = input("Nome: ")
+            try:
+                for jogo in jogos:
+                    if jogo.nome == nome and nome != nome_antigo:
+                        raise NomeInvalidoException
+                break
+            except NomeInvalidoException as e:
+                print(e)
 
-        #return {"nome": nome, "desenvolvedora": desenvolvedora, "genero": genero,
-        # "faixa etaria": faixa_etaria, "preco": preco}
+        desenvolvedora = input("Desenvolvedora: ")
+        genero = input("Genero: ")
+        while True:
+            faixa_etaria = input("Faixa etaria (0 ate 100): ")
+            try:
+                faixa_etaria = int(faixa_etaria)
+                if faixa_etaria > 100 or faixa_etaria < 0:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Selecione um valor inteiro valido")
+
+        while True:
+            preco = input("Preco (0 ate 10000): ")
+            try:
+                preco = float(preco)
+                if preco > 10000 or preco < 0:
+                    raise ValueError
+                break
+            except ValueError:
+                print("Selecione um valor valido")
+
+        return {"nome": nome, "desenvolvedora": desenvolvedora, "genero": genero,
+                "faixa etaria": faixa_etaria, "preco": preco}
 
     def mostrar_jogo(self, dados_jogo):
-        print("Nome do jogo: ", dados_jogo["nome"])
+        print("\nNome do jogo: ", dados_jogo["nome"])
         print("Desenvolvedora do jogo: ", dados_jogo["desenvolvedora"])
         print("Genero do jogo: ", dados_jogo["genero"])
         print("Faixa etaria do jogo: ", dados_jogo["faixa etaria"])
-        print("Preço do jogo: ", dados_jogo["preco"])
+        print("Preço do jogo: ", str(dados_jogo["preco"]) + "\n")
+
+    def escrever_nome(self):
+        nome = input("Nome do jogo: ")
+        return nome
