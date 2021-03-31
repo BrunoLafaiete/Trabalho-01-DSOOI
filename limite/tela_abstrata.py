970194f63f5ca8cd26d1b3d879a59c9dcaafc94a
JoaoPedro2002 <80429973+JoaoPedro2002@users.noreply.github.com>
2021-03-31 19:36:30 -0300

    Tela abstrata. Lembre de adiciona-la.

--- limite/tela_abstrata.py
+++ limite/tela_abstrata.py
@@ -1,10 +1,28 @@
 from abc import ABC, abstractmethod
 
+
 class TelaAbstrata(ABC):
 
-  @abstractmethod
-  def tela_opcoes(self):
-    pass
+    @abstractmethod
+    def tela_opcoes(self):
+        pass
    
-  def le_num_int()
-    pass
+    def le_num_int(self, mensagem: str = "", inteiros_validos: [] = None):
+        while True:
+            valor_lido = input(mensagem)
+            try:
+                inteiro = int(valor_lido)
+                if inteiros_validos and inteiro not in inteiros_validos:
+                    raise ValueError
+                return inteiro
+            except ValueError:
+                print("Valor invalido: Digite um valor numerico inteiro valido")
+                if inteiros_validos:
+                    print("Valores validos: ", inteiros_validos)
+
+    def le_lista(self, lista: [] = None):
+        mensagem = ""
+        for item in lista:
+            mensagem += str(item) + ", "
+
+        return mensagem[:-2]
