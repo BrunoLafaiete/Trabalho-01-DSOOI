533b2324bc4c45308ca11c00d8594f2f0aaec8a2
JoaoPedro2002 <80429973+JoaoPedro2002@users.noreply.github.com>
2021-03-31 19:16:15 -0300

    Update controlador_desenvolvedora.py
    Adicionando a opção de alterar a desenvolvedora e tratando exceções.

--- controle/controlador_desenvolvedora.py
+++ controle/controlador_desenvolvedora.py
@@ -1,25 +1,52 @@
 from entidade.desenvolvedora import Desenvolvedora
 from limite.tela_desenvolvedora import TelaDesenvolvedora
 
-class ControladorDesenvolvedoras:
+
+class ControladorDesenvolvedora:
 
     def __init__(self, controlador_sistema):
         self.__controlador_sistema = controlador_sistema
         self.__desenvolvedoras = []
         self.__tela_desenvolvedora = TelaDesenvolvedora()
+        self.__continua_nesse_menu = True
 
     def abre_tela(self):
-        lista_opcoes = {1: self.cadastra_desenvolvedora, 4: self.lista_desenvolvedoras}
-        continua_nesse_menu = True
+        lista_opcoes = {0: self.voltar, 1: self.cadastra_desenvolvedora, 2: self.alterar_desenvolvedora,
+                        3: self.get_dados_desenvolvedora, 4: self.lista_desenvolvedoras}
+        self.__continua_nesse_menu = True
 
-        while continua_nesse_menu:
+        while self.__continua_nesse_menu:
             lista_opcoes[self.__tela_desenvolvedora.tela_opcoes()]()
 
     def cadastra_desenvolvedora(self):
-        desenvolvedora = Desenvolvedora(self.__tela_desenvolvedora.cadastrar_desenvolvedora())
+        desenvolvedora = Desenvolvedora(self.__tela_desenvolvedora.cadastrar_desenvolvedora(self.__desenvolvedoras))
 
         self.__desenvolvedoras.append(desenvolvedora)
 
+    def alterar_desenvolvedora(self):
+        desenvolvedora = self.get_desenvolvedora_by_nome()
+        nome_antigo = desenvolvedora.nome
+        desenvolvedora.nome = self.__tela_desenvolvedora.alterar_desenvolvedora(self.__desenvolvedoras, nome_antigo)
+
     def lista_desenvolvedoras(self):
         for desenvolvedora in self.__desenvolvedoras:
-            self.__tela_desenvolvedora.mostrar_desenvolvedora({"nome": desenvolvedora.nome, "jogos": desenvolvedora.jogos})
+            self.__tela_desenvolvedora.mostrar_desenvolvedora({"nome": desenvolvedora.nome,
+                                                               "jogos": desenvolvedora.jogos})
+
+    def voltar(self):
+        self.__continua_nesse_menu = False
+
+    def get_dados_desenvolvedora(self):
+        desenvolvedora = self.get_desenvolvedora_by_nome()
+        self.__tela_desenvolvedora.mostrar_desenvolvedora({"nome": desenvolvedora.nome,
+                                                           "jogos": desenvolvedora.jogos})
+
+    def get_desenvolvedora_by_nome(self):
+        nome = self.__tela_desenvolvedora.escrever_nome()
+        for desenvolvedora in self.__desenvolvedoras:
+            if desenvolvedora.nome == nome:
+                return desenvolvedora
+
+    @property
+    def desenvolvedoras(self):
+        return self.__desenvolvedoras
