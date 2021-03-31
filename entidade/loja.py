13d60f39ecd25658d2cfd3864274759cfb10fc9d
JoaoPedro2002 <80429973+JoaoPedro2002@users.noreply.github.com>
2021-03-31 19:11:42 -0300

    Update loja.py
    Adicionado a loja

--- entidade/loja.py
+++ entidade/loja.py
@@ -1,5 +1,18 @@
-
-
-
 class Loja:
-    pass
+    def __init__(self, jogos):
+        self.__jogos = jogos
+        self.__generos = []
+        self.__desenvolvedoras = []
+        for jogo in jogos:
+            if jogo.genero not in self.__generos:
+                self.__generos.append(jogo.genero)
+            #if jogo.desenvolvedora.nome not in self.__desenvolvedoras:
+             #   self.__desenvolvedoras.append(jogo.desenvolvedora.nome)
+
+    @property
+    def jogos(self):
+        return self.__jogos
+
+    @property
+    def generos(self):
+        return self.__generos
