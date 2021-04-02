0a4658627fcdcac693ba9bcd90a71cdd7ffdef7b
JoaoPedro2002 <80429973+JoaoPedro2002@users.noreply.github.com>
2021-04-02 17:08:18 -0300

    Entidades_atualizados

--- entidade/usuario.py
+++ entidade/usuario.py
@@ -18,7 +18,15 @@ class Usuario(Pessoa):
     @saldo.setter
     def saldo(self, saldo: float):
         if isinstance(saldo, float):
-            self.__saldo += saldo
+            self.__saldo = saldo
+
+    def credite(self, valor):
+        if isinstance(valor, float):
+            self.__saldo += valor
+
+    def debite(self, valor):
+        if isinstance(valor, float):
+            self.__saldo -= valor
 
     @property
     def compras(self):
@@ -27,3 +35,9 @@ class Usuario(Pessoa):
     @property
     def jogos(self):
         return self.__jogos
+
+    def incluir_compra(self, compra):
+        self.__compras.append(compra)
+
+    def remover_compra(self, compra):
+        self.__compras.remove(compra)
