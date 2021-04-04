2b4134f3d21f781752f02ac3402a03daca13ca6e
Bruno <78390542+BrunoVzqz@users.noreply.github.com>
2021-04-04 15:46:55 -0300

    Update comunidade.py

--- entidade/comunidade.py
+++ entidade/comunidade.py
@@ -1,10 +1,9 @@
-
-
-
 class Comunidade:
     def __init__(self, nome: str, descricao: str):
         self.__nome = nome
         self.__descricao = descricao
+        self.__jogo = None
+        self.__usuarios = []
 
     @property
     def nome(self):
@@ -22,3 +21,18 @@ class Comunidade:
     def descricao(self, descricao):
         self.__descricao = descricao
 
+    @property
+    def jogo(self):
+        return self.__jogo
+
+    @jogo.setter
+    def jogo(self, jogo):
+        self.__jogo.append(jogo)
+
+    @property
+    def usuarios(self):
+        return self.__usuarios
+
+    @usuarios.setter
+    def usuarios(self, usuario):
+        self.__usuarios.append(usuario)
