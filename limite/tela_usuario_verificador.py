2527d5d8a8600c2ebda6e8e3b445aeeacda238c1
Bruno <78390542+BrunoVzqz@users.noreply.github.com>
2021-05-07 00:39:21 -0300

    Update tela_usuario_verificador.py

--- limite/tela_usuario_verificador.py
+++ limite/tela_usuario_verificador.py
@@ -22,16 +22,5 @@ class TelaUsuarioVerificador(TelaAbstrata):
         self.__window = sg.Window("Verificador", element_justification='center',
                                  finalize=True).Layout(layout)
 
-    '''def tela_opcoes2(self):
-        sg.theme('Reddit')
-        layout = [[sg.Text('Por favor digite os dados solicitados', font=('Helvetica', 10))],
-                  [sg.Text('Email: ', size=('17', '1'), font=('Helvetica', 10)),
-                   sg.InputText(key='email')],
-                  [sg.Text('Senha: ', size=('17', '1'), font=('Helvetica', 10)),
-                   sg.InputText(key='senha')],
-                  [sg.Button('Enviar', font=('Helvetica', 10)), sg.Button('Cancelar', font=('Helvetica', 10))]]
-        self.__window = sg.Window("Verificador", element_justification='center',
-                                 finalize=True).Layout(layout)'''
-
     def close(self):
         self.__window.Close()
