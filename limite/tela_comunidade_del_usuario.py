ec1efb193ff4f36624aea6600334baed3d415676
Bruno <78390542+BrunoVzqz@users.noreply.github.com>
2021-05-07 16:31:17 -0300

    Update tela_comunidade_del_usuario.py

--- limite/tela_comunidade_del_usuario.py
+++ limite/tela_comunidade_del_usuario.py
@@ -7,19 +7,17 @@ class TelaComunidadeDelUsuario(TelaAbstrata):
     def __init__(self):
         self.__window = None
 
-    def open(self, nomes_comunidades_usuario): # -> 'mostra_menu'
-        self.tela_opcoes(nomes_comunidades_usuario)
+    def open(self, comunidades_nome): # -> 'mostra_menu'
+        self.tela_opcoes(comunidades_nome)
         button_key, values = self.__window.Read()
         if button_key is None:
             button_key = 0
         return (button_key, values)
 
-    def tela_opcoes(self, nomes_comunidades_usuario):
-        comunidades = nomes_comunidades_usuario
+    def tela_opcoes(self, comunidades_nome):
         sg.theme('Reddit')
         layout = [[sg.Text('Escolha a comunidade da qual deseja remover o usuario', font=('Helvetica', 10))],
-                  [sg.Text('Comunidades: ', size=(20, 1), font=('Helvetica', 10))],
-                  [sg.Combo(comunidades, key='nome_comunidade')],
+                  [sg.Listbox(comunidades_nome, size=(15, 3), key='comunidade')],
                   [sg.Button('Enviar', font=('Helvetica', 10)), sg.Button('Cancelar', font=('Helvetica', 10))]]
         self.__window = sg.Window("Excluir um usuario da comunidade",
                                  finalize=True).Layout(layout)
